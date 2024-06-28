22a4756fec756f8d47f701b01cbb3abfcc1c308b
tajansari612 <tajansari612@gmail.com>
2024-06-28 14:12:25 +0530

    twrp tree generated

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Infinix-X6815D.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Infinix-X6815D-user \
+    omni_Infinix-X6815D-userdebug \
+    omni_Infinix-X6815D-eng
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# x6815d_twrp_tree
+# Android device tree for Infinix Infinix X6815D (Infinix-X6815D)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
